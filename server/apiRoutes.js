6c19d7c2257eeec3770911da888d453b8322dfdd
Sam Meech-Ward <sam@meech-ward.com>
2019-06-30 17:37:15 +0100

    Log errors from properties endpoint

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -3,8 +3,10 @@ module.exports = function(router, database) {
   router.get('/properties', (req, res) => {
     database.getAllProperties(req.query, 20)
     .then(properties => res.send({properties}))
-    .catch(e => res.send(e));
-    
+    .catch(e => {
+      console.error(e);
+      res.send(e)
+    }); 
   });
 
   router.get('/reservations', (req, res) => {
