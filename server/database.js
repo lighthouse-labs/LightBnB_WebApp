e2de9d63f630cea8963156addd6b104040f1cf9f
Andy Lindsay <andydlindsay@gmail.com>
2023-01-24 15:40:37 -0800

    refactors server code

--- server/database.js
+++ server/database.js
@@ -8,43 +8,36 @@ const users = require('./json/users.json');
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
+const getUserWithEmail = function (email) {
   for (const userId in users) {
-    user = users[userId];
+    const user = users[userId];
     if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
+      return Promise.resolve(user);
     }
   }
-  return Promise.resolve(user);
-}
-exports.getUserWithEmail = getUserWithEmail;
+  return Promise.resolve(null);
+};
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
+const getUserWithId = function (id) {
   return Promise.resolve(users[id]);
-}
-exports.getUserWithId = getUserWithId;
-
+};
 
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
+const addUser = function (user) {
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
   return Promise.resolve(user);
-}
-exports.addUser = addUser;
+};
 
 /// Reservations
 
@@ -53,10 +46,9 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
+const getAllReservations = function (guest_id, limit = 10) {
   return getAllProperties(null, 2);
-}
-exports.getAllReservations = getAllReservations;
+};
 
 /// Properties
 
@@ -66,25 +58,31 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
+const getAllProperties = function (options, limit = 10) {
   const limitedProperties = {};
   for (let i = 1; i <= limit; i++) {
     limitedProperties[i] = properties[i];
   }
   return Promise.resolve(limitedProperties);
-}
-exports.getAllProperties = getAllProperties;
-
+};
 
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
+const addProperty = function (property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
   return Promise.resolve(property);
-}
-exports.addProperty = addProperty;
+};
+
+module.exports = {
+  getUserWithEmail,
+  getUserWithId,
+  addUser,
+  getAllReservations,
+  getAllProperties,
+  addProperty
+};
--- server/server.js
+++ server/server.js
@@ -1,38 +1,32 @@
-const database = require('./database');
-const apiRoutes = require('./apiRoutes');
-const userRoutes = require('./userRoutes');
-
 const path = require('path');
-
 const express = require('express');
 const cookieSession = require('cookie-session');
-const bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 3000; 
 
+// middleware
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieSession({
   name: 'session',
   keys: ['key1']
 }));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const apiRoutes = require('./routes/apiRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 // /api/endpoints
-const apiRouter = express.Router();
-apiRoutes(apiRouter, database);
-app.use('/api', apiRouter);
+app.use('/api', apiRoutes);
 
 // /user/endpoints
-const userRouter = express.Router();
-userRoutes(userRouter, database);
-app.use('/users', userRouter);
-
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/users', userRoutes);
 
 app.get("/test", (req, res) => {
   res.send("🤗");
 });
 
-const port = process.env.PORT || 3000; 
-app.listen(port, (err) => console.log(err || `listening on port ${port} 😎`));
+app.listen(port, (err) => {
+  console.log(err || `listening on port ${port} 😎`);
+});
